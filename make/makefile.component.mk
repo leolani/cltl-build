8ba46ac659c3ac211d0c122b17d8fe86d02fdbaf
Thomas Baier <tbaier@math.bme.hu>
2021-02-17 22:50:07 +0100

    Add makefiles

---
+++ make/makefile.component.mk
@@ -0,0 +1,25 @@
+SHELL = /bin/bash
+
+project_root ?= $(realpath ..)
+project_name = $(notdir $(realpath .))
+project_version = $(shell cat version.txt)
+project_repo ?= ${project_root}/cltl-requirements/leolani
+project_mirror ?= ${project_root}/cltl-requirements/mirror
+
+# Add required components
+dependencies = $(addprefix $(project_root)/, \
+	cltl-requirements \
+	cltl-component)
+
+.DEFAULT_GOAL := install
+
+include $(project_root)/$(project_name)/*.mk
+
+
+clean: py-clean
+
+install: py-install
+
+.PHONY: docker
+docker: py-install
+	DOCKER_BUILDKIT=1 docker build -t cltl/${project_name}:${project_version} .
