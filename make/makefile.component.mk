4fff0c76f37bb728e4f8929b4ecf6e2650f2cfed
Thomas Baier <thomas.baier@innovid.com>
2026-05-17 21:10:18 +0200

    Handle unallowed characters

--- make/makefile.component.mk
+++ make/makefile.component.mk
@@ -2,6 +2,7 @@ SHELL = /bin/bash
 
 project_name ?= $(notdir $(realpath .))
 project_version ?= $(shell cat VERSION)
+docker_version ?= $(shell cat VERSION | tr '+!' '--')
 
 
 clean: py-clean
@@ -10,4 +11,4 @@ install: docker
 
 .PHONY: docker
 docker:
-	DOCKER_BUILDKIT=1 docker build -t cltl/${project_name}:${project_version} -t cltl/${project_name}:latest .
+	DOCKER_BUILDKIT=1 docker build -t cltl/${project_name}:${docker_version} -t cltl/${project_name}:latest .
